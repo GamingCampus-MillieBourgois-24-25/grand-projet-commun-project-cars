05eee4947cc56d2bc51bf3b8bf293b1edacc0493
CatChow0 <thetrueshibadoggo@gmail.com>
2025-04-09 11:48:58 +0200

    Minor Update - Vroom v0.12.0

--- Assets/01-Script/gamemanager.cs
+++ Assets/01-Script/gamemanager.cs
@@ -1,43 +1,23 @@
 using UnityEngine;
+using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
     // Singleton instance
     public static GameManager Instance { get; private set; }
 
-    // Données persistantes
-    public int VoitureSelectionneeIndex { get; private set; }
-    public GameObject[] ModelesVoitures { get; private set; }
-
-    private void Awake()
+    // Game Manager Persistence
+    void Awake()
     {
-        // Implémentation du pattern Singleton
+        // Singleton pattern
         if (Instance == null)
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);
-            ModelesVoitures = Resources.LoadAll<GameObject>("Voitures");
         }
         else
         {
             Destroy(gameObject);
         }
-    }
-
-    // Définir la voiture sélectionnée
-    public void DefinirVoitureSelectionnee(int index)
-    {
-        VoitureSelectionneeIndex = index;
-    }
-
-    // Obtenir la voiture sélectionnée
-    public GameObject ObtenirVoitureSelectionnee()
-    {
-        if (ModelesVoitures != null && ModelesVoitures.Length > 0 && 
-            VoitureSelectionneeIndex >= 0 && VoitureSelectionneeIndex < ModelesVoitures.Length)
-        {
-            return ModelesVoitures[VoitureSelectionneeIndex];
-        }
-        return null;
     }
 }
