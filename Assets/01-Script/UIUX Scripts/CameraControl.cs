82efaae4c2fcb11d14d1ece1ae1076c116016c81
Kagutsuchi84 <115561782+Mattys8423@users.noreply.github.com>
2025-04-18 20:28:44 +0200

    fix uncompleted

--- Assets/01-Script/UIUX Scripts/CameraControl.cs
+++ Assets/01-Script/UIUX Scripts/CameraControl.cs
@@ -2,28 +2,27 @@ using UnityEngine;
 
 public class CameraControl : MonoBehaviour
 {
-    [SerializeField] private Transform target;  // La voiture
-    public float rotationSpeed = 5f; // Vitesse de rotation
-    private float currentRotationX = 0f; // Pour suivre l'angle actuel de rotation autour de la voiture
+    [SerializeField] private Transform target;
+    public float rotationSpeed = 5f;
+    private float currentRotationX = 0f;
 
     void Update()
     {
-        // Détection du mouvement de la souris ou du doigt
-        float horizontal = Input.GetAxis("Mouse X");  // Souris sur PC
-        float vertical = Input.GetAxis("Mouse Y");    // Souris sur PC
+        float horizontal = Input.GetAxis("Mouse X");
+        float vertical = Input.GetAxis("Mouse Y");  
 
-        // Pour mobile (touches)
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
-            horizontal = touch.deltaPosition.x * 0.1f;  // Sensibilité du glissement horizontal
-            // vertical = touch.deltaPosition.y * 0.1f;  // ON SUPPRIME ce contrôle
+            horizontal = touch.deltaPosition.x * 0.1f;
         }
 
-        // Rotation horizontale
         transform.RotateAround(target.position, Vector3.up, horizontal * rotationSpeed);
-
-        // On garde l'angle actuel de la rotation verticale (pas de mouvement haut/bas)
         transform.rotation = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, 0);
+    }
+
+    public void SetRotationSpeed(float Speed)
+    {
+        rotationSpeed = Speed;
     }
 }
--- Assets/01-Script/UIUX Scripts/ScrollSnapToCenter.cs
+++ Assets/01-Script/UIUX Scripts/ScrollSnapToCenter.cs
@@ -2,9 +2,14 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
 using TMPro;
+using System.Collections;
+using System;
 
 public class ScrollSnapToCenter : MonoBehaviour
 {
+    [SerializeField] CustomizeCar script;
+    [SerializeField] Button Next;
+    [SerializeField] Button Previous;
     public ScrollRect scrollRect;
     public RectTransform content;
     public RectTransform SampleListItem;
@@ -14,6 +19,7 @@ public class ScrollSnapToCenter : MonoBehaviour
     public string[] ItemNames;
 
     private bool IsSnapped;
+    private bool CanChange = false;
 
     public float snapForce;
     public int ItemNumber;
@@ -27,7 +33,6 @@ public class ScrollSnapToCenter : MonoBehaviour
     void Update()
     {
         ItemNumber = Mathf.RoundToInt((0 - content.localPosition.x / (SampleListItem.rect.width + horizontalLayoutGroup.spacing)));
-        Debug.Log(ItemNumber);
 
         if (scrollRect.velocity.magnitude < 200)
         {
@@ -41,6 +46,7 @@ public class ScrollSnapToCenter : MonoBehaviour
             if (content.localPosition.x == 0 - (ItemNumber) * (SampleListItem.rect.width + horizontalLayoutGroup.spacing))
             {
                 IsSnapped = true;
+                CanChange = true;
             }
         }
         if (scrollRect.velocity.magnitude > 200)
@@ -49,22 +55,142 @@ public class ScrollSnapToCenter : MonoBehaviour
             IsSnapped = false;
             snapSpeed = 0;
         }
+        if (scrollRect.velocity.magnitude > 0)
+        {
+            CanChange = false;
+        }
     }
 
-    public void NextItem()
+    //Car
+    public void NextItemCar()
     {
+        Next.interactable = false;
         if (ItemNumber < ItemNames.Length - 1)
         {
             scrollRect.velocity = new Vector2(-1350f, 0f);
+            StartCoroutine(change(script.NextCar, Next));
         }
     }
 
-    public void PreviousItem()
+    public void PreviousItemCar()
     {
+        Previous.interactable = false;
         if (ItemNumber > 0)
         {
             scrollRect.velocity = new Vector2(1350f, 0f);
+            StartCoroutine(change(script.PreviousCar, Previous));
         }
     }
 
+    //Roues
+    public void NextItemRoues()
+    {
+        Next.interactable = false;
+        if (ItemNumber < ItemNames.Length - 1)
+        {
+            scrollRect.velocity = new Vector2(-1350f, 0f);
+            StartCoroutine(change(script.NextRouesMaterial, Next));
+        }
+    }
+
+    public void PreviousItemRoues()
+    {
+        Previous.interactable = false;
+        if (ItemNumber > 0)
+        {
+            scrollRect.velocity = new Vector2(1350f, 0f);
+            StartCoroutine(change(script.PreviousRouesMaterial, Previous));
+        }
+    }
+
+    //Vitres
+    public void NextItemVitres()
+    {
+        Next.interactable = false;
+        if (ItemNumber < ItemNames.Length - 1)
+        {
+            scrollRect.velocity = new Vector2(-1350f, 0f);
+            StartCoroutine(change(script.NextVitresMaterial, Next));
+        }
+    }
+
+    public void PreviousItemVitres()
+    {
+        Previous.interactable = false;
+        if (ItemNumber > 0)
+        {
+            scrollRect.velocity = new Vector2(1350f, 0f);
+            StartCoroutine(change(script.PreviousVitresMaterial, Previous));
+        }
+    }
+
+    //Accessory
+    public void NextItemAccessory()
+    {
+        Next.interactable = false;
+        if (ItemNumber < ItemNames.Length - 1)
+        {
+            scrollRect.velocity = new Vector2(-1350f, 0f);
+            StartCoroutine(change(script.NextAccessoiresMaterial, Next));
+        }
+    }
+
+    public void PreviousItemAccessory()
+    {
+        Previous.interactable = false;
+        if (ItemNumber > 0)
+        {
+            scrollRect.velocity = new Vector2(1350f, 0f);
+            StartCoroutine(change(script.PreviousAccessoiresMaterial, Previous));
+        }
+    }
+
+    //Material
+    public void NextItemMaterial()
+    {
+        Next.interactable = false;
+        if (ItemNumber < ItemNames.Length - 1)
+        {
+            scrollRect.velocity = new Vector2(-1350f, 0f);
+            StartCoroutine(change(script.NextCarrosserieMaterial, Next));
+        }
+    }
+
+    public void PreviousItemMaterial()
+    {
+        Previous.interactable = false;
+        if (ItemNumber > 0)
+        {
+            scrollRect.velocity = new Vector2(1350f, 0f);
+            StartCoroutine(change(script.PreviousCarrosserieMaterial, Previous));
+        }
+    }
+
+    //Phares
+    public void NextItemPhares()
+    {
+        Next.interactable = false;
+        if (ItemNumber < ItemNames.Length - 1)
+        {
+            scrollRect.velocity = new Vector2(-1350f, 0f);
+            StartCoroutine(change(script.NextPharesMaterial, Next));
+        }
+    }
+
+    public void PreviousItemPhares()
+    {
+        Previous.interactable = false;
+        if (ItemNumber > 0)
+        {
+            scrollRect.velocity = new Vector2(1350f, 0f);
+            StartCoroutine(change(script.PreviousPharesMaterial, Previous));
+        }
+    }
+
+    IEnumerator change(Action action, Button bouton)
+    {
+        yield return new WaitForSeconds(1);
+        action();
+        bouton.interactable = true;
+    }
 }
